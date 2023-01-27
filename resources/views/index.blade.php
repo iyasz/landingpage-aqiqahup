522d13fd119ee93b2d2b7b0c929a35c7f84a7bb3
Yasz Avellia <zakamaragames@gmail.com>
2023-01-27 09:04:23 +0700

    menambah navbar,home dan sedikit about

---
+++ resources/views/index.blade.php
@@ -0,0 +1,92 @@
+@extends('layout.mainlayout')
+
+@section('content')
+    <section id="home">
+        <div class="container">
+            <div class="row mt-5">
+                <div class="col-lg-8">
+                    <div class="home-main mt-">
+                        <div class="text-info-main px-2 py-1 rounded-2">
+                            <p class="mb-0 fs-s-sm fw-300-p"><i class="bi bi-caret-down-fill"></i> Aqiqah Terpercaya dan
+                                Terbaik!</p>
+                        </div>
+                        <h1 class="text-main fw-700-p">Pengusaha Aqiqah <br> Itu Berani <span> SCALE UP!</span></h1>
+                        <p class="fw-300-p fs-sm mt-4 opacity-75">Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                            illo porro provident <br> blanditiis est, amet autem voluptatem minima magnam a sed dolore <br>
+                            dolores Cupiditate, a? Aliquid, asperiores totam! .</p>
+                        <div class="btn-main">
+                            <a href="https://app.aqiqahup.id/register?ref=website"
+                                class="btn btn-primary btn-daftar fw-500-p py-2">Daftar Sekarang <i
+                                    class="bi bi-arrow-right-short"></i></a>
+                            <a href="" class="btn btn-info btn-play ms-3 fw-500-p "><i
+                                    class="bi bi-play-circle me-2"></i> Mulai</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4">
+                    <div class="row justify-content-center">
+                        <img width="330px" class="rounded-circle" src="/assets/img/mainimg.png" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section id="quality">
+        <div class="container">
+            <div class="row">
+
+            </div>
+        </div>
+    </section>
+
+    <section class="mt-5" id="about">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <div class="text-center">
+                        <div class=" d-flex justify-content-center">
+                            <div class="text-info-main2 px-2 py-1 rounded-2">
+                                <p class="mb-0 fs-s-sm fw-300-p">#AqiqahUP</p>
+                            </div>
+                        </div>
+                        <h1 class="fw-700-p mt-2">Sofware Andalan Para <br> Pengusaha Aqiqah</h1>
+                        <p class="fs-6 opacity-75 mt-3">Aqiqahup.id adalah aplikasi terlengkap dalam memahami setiap bisnis
+                            proses pemesanan, <br> pengiriman sampai dengan laporan keuangan lembaga Aqiqah.</p>
+                    </div>
+                </div>
+                <div class="col-12 mt-3">
+                    <div class="row justify-content-center text-center">
+                        <div class="col-lg-4 col-md-6 col-11">
+                            <div class="card-about">
+                                <i class="bi bi-chat-square-text icon-about"></i>
+                                <h6 class="text-about">Fitur Formulir Pemesanan Yang Lengkap</h6>
+                                <p class="my-3 text-start">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempore
+                                    quidem nemo exercitationem, delectus in tempora numquam eos dignissimos hic nihil rem
+                                    nostrum assumenda at odio.</p>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-md-6 col-11">
+                            <div class="card-about">
+                                <i class="bi bi-bank icon-about"></i>
+                                <h6 class="text-about">Management order yang terpusat dan mudah dimengerti</h6>
+                                <p class="my-3 text-start">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempore
+                                    quidem nemo exercitationem, delectus in tempora numquam eos dignissimos hic nihil rem
+                                    nostrum assumenda at odio.</p>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-md-6 col-11">
+                            <div class="card-about">
+                                <i class="bi bi-database-exclamation icon-about"></i>
+                                <h6 class="text-about">Laporan akuntasi yang lengkap dan otomatis</h6>
+                                <p class="my-3 text-start">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempore
+                                    quidem nemo exercitationem, delectus in tempora numquam eos dignissimos hic nihil rem
+                                    nostrum assumenda at odio.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+@endsection
---
+++ resources/views/layout/mainlayout.blade.php
@@ -0,0 +1,96 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>AqiqahUP - Aqiqah ready to UP!</title>
+
+    {{-- bootstrap  --}}
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+
+    {{-- icon bootstrap  --}}
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
+
+    {{-- font google  --}}
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;500;600&family=Roboto:wght@100;300;400;500;700&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="/assets/css/custom.css">
+  </head>
+  <body>
+
+    <div class="container mt-2 d-lg-block d-md-none d-none">
+        <div class="row">
+            <div class="col-3">
+                <a class="mb-0 text-decoration-none text-gray-50 fs-sm link-top" href="mailto:halo@aqiqahup.id"><i class="bi bi-chat-left-text"></i> halo@aqiqahup.id</a>
+            </div>
+            <div class="col-3">
+                <a href="tel:+62215852475" class="mb-0 text-decoration-none text-gray-50 fs-sm link-top"><i class="bi bi-telephone"></i> +62215852475</a>
+            </div>
+        </div>
+    </div>
+    <hr class="mb-0 hr-nav d-lg-block d-md-none d-none">
+        {{-- <nav class="navbar sticky-top navbar-expand-lg navbar-light bg-white py-3 shadow-sm">
+            <div class="container"><a class="navbar-brand" href="">
+                <strong class="h6 mb-0 font-weight-bold text-uppercase"><img class="" width="170px" src="/assets/img/logo.svg" alt=""></strong></a>
+                <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav ml-auto">
+                        <li class="nav-item active"><a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a></li>
+                        <li class="nav-item"><a class="nav-link" href="#">Link</a></li>
+                        <li class="nav-item"><a class="nav-link disabled" href="#">Disabled</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav> --}}
+        <nav class="navbar navbar-expand-lg py-3 bg-white shadow-sm sticky-top">
+            <div class="container">
+              <a class="navbar-brand" href="#"><img class="" width="170px" src="/assets/img/logo.svg" alt=""></a>
+              <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+              <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+                <div class="offcanvas-header">
+                  <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Offcanvas</h5>
+                  <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                </div>
+                <div class="offcanvas-body">
+                  <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+                    <li class="nav-item mx-1">
+                      <a class="nav-link fw-400-r" href="#">Home</a>
+                    </li>
+                    <li class="nav-item mx-1">
+                      <a class="nav-link fw-400-r" href="#">About Us</a>
+                    </li>
+                    <li class="nav-item mx-1">
+                      <a class="nav-link fw-400-r" href="#">Harga</a>
+                    </li>
+                    <li class="nav-item mx-1">
+                      <a class="nav-link fw-400-r" href="#">Contact</a>
+                    </li>
+                    <li class="nav-item mx-1">
+                      <a class="nav-link fw-400-r" href="https://aqiqahup.tawk.help/">Bantuan</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                      <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        <i class="bi bi-brightness-high-fill"></i>
+                      </a>
+                      <ul class="dropdown-menu dropdown-menu-end" >
+                        <li><a class="dropdown-item" href="#"><i class="bi bi-brightness-high-fill"></i> Light</a></li>
+                        <li><a class="dropdown-item" href="#"><i class="bi bi-moon-stars-fill"></i> Dark</a></li>
+                      </ul>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </nav>
+
+        @yield('content')
+  
+
+    {{-- bootstrap  --}}
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+  </body>
+</html>
